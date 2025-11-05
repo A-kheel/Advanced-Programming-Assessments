88651f264d17b4c5551120f12e01e44fd09a2321
A-kheel <aquilvienjose@gmail.com>
2025-11-05 10:41:44 +0400

    Add files via upload

--- python/Excercise 3/students.py
+++ python/Excercise 3/students.py
@@ -17,51 +17,86 @@ def start():
             "scores": scores,
             "total": total
         })
+        
+    for s in students:
+        cw = sum(s["scores"])
+        pct = (s["total"] / 160) * 100
+        if pct >= 70:
+            s["grade"] = "A"
+        elif pct >= 60:
+            s["grade"] = "B"
+        elif pct >= 50:
+            s["grade"] = "C"
+        elif pct >= 40:
+            s["grade"] = "D"
+        else:
+            s["grade"] = "F"
     return students
 
+def results(marks):
+    cw = sum(marks["grade"])
+    exam = marks["exam"]
+    total = cw + exam
+    pct = (total / 160) * 100
+    if pct >= 70:
+        grade = "A"
+    elif pct >= 60:
+        grade = "B"
+    elif pct >= 50:
+        grade = "C"
+    elif pct >= 40:
+        grade = "D"
+    else:
+        grade = "F"
+    return grade
+
 def view_all():
-    frame3_text.delete(1.0, END)
+    frame3text.delete(1.0, END)
     for s in students:
-        frame3_text.insert(END,
+        frame3text.insert(END,
             f"Number: {s['number']}\n"
             f"Name: {s['name']}\n"
             f"Scores: {s['scores']}\n"
             f"Total: {s['total']}\n"
-            
+            f"Grade: {s['grade']}\n\n"
         )
 
 def highest():
-    frame3_text.delete(1.0, END)
+    frame3text.delete(1.0, END)
     highest = max(students, key=lambda x: x['total'])
-    frame3_text.insert(END,
+    frame3text.insert(END,
         f"Highest:\n"
         f"Name: {highest['name']}\n"
         f"Number: {highest['number']}\n"
         f"Scores: {highest['scores']}\n"
         f"Total: {highest['total']}\n"
+        f"Grade: {highest['grade']}\n"
     )
 
 def lowest():
-    frame3_text.delete(1.0, END)
+    frame3text.delete(1.0, END)
     lowest = min(students, key=lambda x: x['total'])
-    frame3_text.insert(END,
+    frame3text.insert(END,
         f"Lowest:\n"
         f"Name: {lowest['name']}\n"
         f"Number: {lowest['number']}\n"
         f"Scores: {lowest['scores']}\n"
         f"Total: {lowest['total']}\n"
+        f"Grade: {lowest['grade']}\n"
     )
 
 def search():
-    frame3_text.delete(1.0, END)
-    sid = ent.get().strip()
+    frame3text.delete(1.0, END)
+    snumber = ent.get().strip()
     for s in students:
-        if s['number'] == sid:
-            frame3_text.insert(END,
+        if s['number'].strip() == snumber:
+            frame3text.insert(END,
                 f"{s['name']} (Number: {s['number']}):\n"
                 f"Scores: {s['scores']}\n"
                 f"Total: {s['total']}\n"
+                f"Grade: {s['grade']}\n"
             )
+    
 
 root = Tk()
 root.geometry('700x600')
@@ -97,8 +132,8 @@ rec.pack(side="left", padx=10)
 frame3 = Frame(root, bg='white', bd=2, relief=SOLID)
 frame3.pack(padx=20, pady=20, fill=BOTH, expand=True)
 
-frame3_text = Text(frame3, wrap=WORD, font=("Arial", 12))
-frame3_text.pack(fill=BOTH, expand=True, padx=10, pady=10)
+frame3text = Text(frame3, wrap=WORD, font=("Arial", 12), bd=0, borderwidth=0, relief=FLAT)
+frame3text.pack(fill=BOTH, expand=True, padx=10, pady=10)
 
 students = start()
 
